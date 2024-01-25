34b39664b174237bd2136e9aeb6d8cf28a438a6b
Omegabug <vial.jeanvial@gmail.com>
2024-01-25 14:43:07 +0100

    Better working form
    The information is saved and a simple test is made

--- src/Components/form.jsx
+++ src/Components/form.jsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
-import PropTypes from 'prop-types';
+import "./form.css";
+import userIcon from "./Assets/user.svg";
+import lockIcon from "./Assets/lock.svg";
 
+export const LoginForm = (props) => {
+  const [userName,setUserName] = useState("");
+  const [password,setPassword] = useState("");
 
-export const LoginForm = (prop) => {
+  function handleSubmit  () {
+    console.log(userName);
+    if ( userName =="test@luxpmsoft.com" && password == "test1234!")
+      console.log("connected");
+    else
+      console.log("error");
+  }
+
   return (
-    <form onSubmit={prop.onSubmit} >
-
-      <label><div className="text-wrapper">{prop.label}</div></label>
-      <input type={prop.types}  required={prop.required} />
-      <input type='submit' />
-
+    <form >
+      <div className="username">
+        <div className="overlap-group-2">
+          <img className="img" alt="User" src={userIcon} />
+          <input value = {userName} onChange={(e) => setUserName(e.target.value)} type="text" id="userName" placeholder="USERNAME" required={true} />
+        </div>
+      </div>
+      <div className="password">
+        <div className="overlap-group-2">
+          <img className="img" alt="Lock" src={lockIcon} />
+          <input value = {password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" placeholder="PASSWORD" required={true} />
+        </div>
+      </div>
+      <button className="login-btn" type="submit" onClick={handleSubmit} >
+        <div className="login-wrapper">
+          <div className="login">LOGIN</div>
+        </div>
+      </button>
     </form>
   );
 }
-
-LoginForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-  label: PropTypes.string.isRequired,
-  types: PropTypes.string.isRequired,
-  required: PropTypes.bool.isRequired,
-};
 
 export default LoginForm;
--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -1,21 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 import "./form.css";
-import userIcon from "./Assets/user.svg";
-import lockIcon from "./Assets/lock.svg";
 import cartIcon from "./Assets/cart.svg";
 import vector from "./Assets/vector.svg";
 import {LoginForm} from './form';
 
 export const Login = () => {
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log('Form was submitted');
-  }
   return (
     <div className="dashboard-login">
+
       <div className="overlap-wrapper">
+
         <div className="overlap">
+
           <div className="BG">
             <div className="overlap-group">
               <div className="ellipse" />
@@ -23,33 +20,41 @@ export const Login = () => {
             </div>
             <img className="vector" alt="Vector" src={vector}/>
           </div>
-         
+
+          <div className="form">
+          <img className="group" alt="group" src={cartIcon} />
+
+            <LoginForm/>
+          </div>
+
+
+          {/*}
           <div className="form">           
               <div className="username">
                 <div className="overlap-group-2">
 
                   <img className="img" alt="User" src={userIcon} />
+                  
                   <form onSubmit={handleSubmit} >
 
-                    <input placeholder="USERNAME" />
-
+                    <input value ={userName} placeholder="USERNAME"   />
                   </form>
                     
                 </div>    
                
               </div>
               <div className="password">
-                  <div className="overlap-group-2">
-                    <img className="img" alt="Lock" src={lockIcon} />
-                    <form onSubmit={handleSubmit} >
+                <div className="overlap-group-2">
+                  <img className="img" alt="Lock" src={lockIcon} />
+                  <form onSubmit={handleSubmit} >
 
-                      <input placeholder="PASSWORD" />
+                    <input placeholder="PASSWORD" />
 
-                    </form>
-                  </div>
+                  </form>
                 </div>
+              </div>
             
-            <button className="login-btn">
+            <button className="login-btn" onClick={handleSubmit}>
               <div className="login-wrapper">
                 <div className="login">LOGIN</div>
               </div>
@@ -57,6 +62,7 @@ export const Login = () => {
             <div className="text-wrapper-2">Forgot password?</div>
             <img className="group" alt="group" src={cartIcon} />
           </div>
+          */}
         </div>
       </div>
     </div>
